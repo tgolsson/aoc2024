e0443adc01a3d13838b6b47aa373f130e47e9ba5
Tom Solberg <me@sbg.dev>
2024-12-02 00:47:33 +0100

    lint

--- src/aoc2024/day1.clj
+++ src/aoc2024/day1.clj
@@ -1,6 +1,9 @@
 (ns aoc2024.day1
-  (:require [clojure.string :as str]
+  (:require [clojure.java.io]
+            [clojure.string :as str]
             [aoc2024.util :refer :all]))
+
+(def example "3 4\n4 3\n2 5\n1 3\n3 9\n3 3")
 
 (defn parse [input-reader]
   (->> input-reader
@@ -17,8 +20,8 @@
           (map #(* % (count-occurrences % right)))
           (reduce +))]))
 
-(def example "3 4\n4 3\n2 5\n1 3\n3 9\n3 3")
-(defn -main [& args]
+(defn -main []
   (assert (= (solve (string-reader example)) [11 31]))
+
   (with-open [rdr (clojure.java.io/reader "input/day1")]
     (println (solve rdr))))
