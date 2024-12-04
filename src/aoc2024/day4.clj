0240e31a0acba45b69a3f0c52db5f720e4f2ba1c
Tom Solberg <me@sbg.dev>
2024-12-04 16:42:40 +0100

    clean up

--- src/aoc2024/day4.clj
+++ src/aoc2024/day4.clj
@@ -25,9 +25,13 @@
 
 (defn convolve [[k1 k2] kernel coll & {:keys [stride] :or {stride 1}}]
   (->> coll
-       (partition k1 stride nil)
-       (map (fn [%] (zip (map #(partition k2 stride nil %) %))))
-       (map #(map kernel %))))
+       (partition k1 stride)
+       (mapv (fn [%] (zip (mapv #(mapv vec (partition k2 stride %)) %))))
+       (mapv #(map kernel %))))
+
+(defn sum
+  ([coll]     (reduce + coll))
+  ([val coll] (reduce + val coll)))
 
 (defn solve [reader]
   (let [listified (->> reader
@@ -35,36 +39,31 @@
                        (map char-array)
                        (map #(into [] %)))]
 
-    [(+ (->> (concat listified (apply transpose listified))
-             (map #(->> %
-                        (partition 4 1 [0])
-                        (filter is-xmas)
-                        count))
-             (reduce +))
-        (->> listified
-             (convolve [4  4] #(if (> (count %) 3) (match [(first %) (second %) (nth % 2) (nth % 3)]
-                                                     [([\X _ _ \S] :seq) ([_ \M \A _] :seq) ([_ \M \A _] :seq)  ([\X _ _ \S] :seq)] 2
-                                                     [([\S _ _ \S] :seq) ([_ \A \A _] :seq) ([_ \M \M _] :seq)  ([\X _ _ \X] :seq)] 2
-                                                     [([\S _ _ \X] :seq) ([_ \A \M _] :seq) ([_ \A \M _] :seq)  ([\S _ _ \X] :seq)] 2
-                                                     [([\X _ _ \X] :seq) ([_ \M \M _] :seq) ([_ \A \A _] :seq)  ([\S _ _ \S] :seq)] 2
-                                                     [([\X _ _ _] :seq) ([_ \M _ _] :seq) ([_ _ \A _] :seq)  ([_ _ _ \S] :seq)] 1
-                                                     [([\S _ _ _] :seq) ([_ \A _ _] :seq) ([_ _ \M _] :seq)  ([_ _ _ \X] :seq)] 1
-                                                     [([_ _ _ \X] :seq) ([_ _ \M _] :seq) ([_ \A _ _] :seq)  ([\S _ _ _] :seq)] 1
-                                                     [([_ _ _ \S] :seq) ([_ _ \A _] :seq) ([_ \M _ _] :seq)  ([\X _ _ _] :seq)] 1
-                                                     :else 0) 0))
-             flatten
-             (reduce +)))
-     (->> listified
-          (convolve [3 3] #(if (> (count %) 2)
-                             (match [(first %) (second %) (nth % 2)]
-                               [([\M _ \S] :seq) ([_ \A _] :seq) ([\M _ \S] :seq)] 1
-                               [([\S _ \S] :seq) ([_ \A _] :seq) ([\M _ \M] :seq)] 1
-                               [([\S _ \M] :seq) ([_ \A _] :seq) ([\S _ \M] :seq)] 1
-                               [([\M _ \M] :seq) ([_ \A _] :seq) ([\S _ \S] :seq)] 1
-                               :else 0)
-                             0))
+    [(->> [(convolve [4 1] #(if (is-xmas (into [] (flatten %))) 1 0) listified)
+           (convolve [1 4] #(if (is-xmas (into [] (flatten %))) 1 0) listified)
+           (convolve [4  4] (fn [[a b c d]]
+                              (match [a b c d]
+                                [[\X _ _ \S] [_ \M \A _] [_ \M \A _] [\X _ _ \S]] 2
+                                [[\S _ _ \S] [_ \A \A _] [_ \M \M _] [\X _ _ \X]] 2
+                                [[\S _ _ \X] [_ \A \M _] [_ \A \M _] [\S _ _ \X]] 2
+                                [[\X _ _ \X] [_ \M \M _] [_ \A \A _] [\S _ _ \S]] 2
+                                [[\X _ _ _] [_ \M _ _] [_ _ \A _] [_ _ _ \S]] 1
+                                [[\S _ _ _] [_ \A _ _] [_ _ \M _] [_ _ _ \X]] 1
+                                [[_ _ _ \X] [_ _ \M _] [_ \A _ _] [\S _ _ _]] 1
+                                [[_ _ _ \S] [_ _ \A _] [_ \M _ _] [\X _ _ _]] 1
+                                :else 0)) listified)]
           flatten
-          (reduce +))]))
+          sum)
+     (->> listified
+          (convolve [3 3] (fn [[a b c]]
+                            (match [a b c]
+                              [[\M _ \S]  [_ \A _] [\M _ \S]] 1
+                              [[\S _ \S]  [_ \A _] [\M _ \M]] 1
+                              [[\S _ \M]  [_ \A _] [\S _ \M]] 1
+                              [[\M _ \M]  [_ \A _] [\S _ \S]] 1
+                              :else 0)))
+          flatten
+          sum)]))
 
 (defn -main []
   (assert (= (inspect (solve (string-reader example))) [18 9]))
